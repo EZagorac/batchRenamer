4596f5e9e10617570da4d68fac88a0b83e2dc8b8
Zagorax <gigakyu@gmail.com>
2018-09-16 19:49:50 -0400

    init

--- batchRenamer.java
+++ batchRenamer.java
@@ -1,39 +1,51 @@
 import java.util.Scanner;
 import java.io.*;
+import javax.swing.*;
+import java.awt.*;
 
 public class batchRenamer{
     public static void main(String[] args){
+        /*
+        JFrame mainWindow = new JFrame("Test");
+
+        mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainWindow.setSize(800,800);
+        mainWindow.pack();
+        mainWindow.setVisible(true);*/
+        rename();
+    }    
+
+    public static void rename(){
         String daSeq = "";
         boolean rename = false;
         String now = "";
-
+        int dot = 0;
+    
         Scanner sc = new Scanner(System.in);
-
+    
         System.out.println("Enter the directory of the files you would like to rename: ");
         File dir = new File(sc.nextLine());
         File[] fileList = dir.listFiles();
         
         System.out.println("Enter the new name for the files: ");
         String newName = sc.nextLine();
-
+    
         for (int i = 1 ; i < fileList.length+1 ; i++){
             String cur = fileList[i-1].getName();
-
-            for (File f : fileList)
-                System.out.println(f);
-
-
+            String[] split = cur.split("\\.");
+            String ext = "." + split[split.length-1];
+    
             String oG = dir + "/" + fileList[i-1].getName();
-
+    
             if (i < 10){
-                now = newName + "-0" + i + ".html";
-                daSeq = dir + "/" + newName + "-0" + i + ".html";
+                now = newName + " - 0" + i + ext;
+                daSeq = dir + "/" + newName + " - 0" + i + ext;
             }
             else{
-                now = newName + "-" + i + ".html";
-                daSeq = dir + "/" + newName + "-" + i + ".html";
+                now = newName + " - " + i + ext;
+                daSeq = dir + "/" + newName + " - " + i + ext;
             }
-
+    
             File file = new File(oG);
             File newFile = new File(daSeq);
             
@@ -43,10 +55,13 @@ public class batchRenamer{
             }
             
             if (rename){
-                if(file.renameTo(newFile)) {
+                try {
+                    file.renameTo(newFile);
                     System.out.println(cur + " has been renamed to " + now);
-                } else {
+                }
+                catch (Exception e) {
                     System.out.println("Rename Failed");
+                    break;
                 }
             }
             else{
@@ -55,3 +70,4 @@ public class batchRenamer{
         }
     }
 }
+
